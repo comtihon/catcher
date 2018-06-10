c5e2a754af28c8c276ee3cae162a6eeeada0439a
Val <valerii.tikhonov@gmail.com>
2018-06-10 11:59:43 +0200

    fix stop tests

--- test/steps/stop_test.py
+++ test/steps/stop_test.py
@@ -1,7 +1,5 @@
 from os.path import join
 
-import psycopg2
-
 from catcher.core.runner import Runner
 from test.abs_test_class import TestClass
 
@@ -10,122 +8,50 @@ class StopTest(TestClass):
     def __init__(self, method_name):
         super().__init__('stop', method_name)
 
-    @property
-    def conf(self):
-        return "dbname=test user=test host=localhost password=test port=5433"
-
-    def setUp(self):
-        super().setUp()
-        conn = psycopg2.connect(self.conf)
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer);")
-        cur.execute("CREATE TABLE migration (id serial PRIMARY KEY, hash varchar(255));")
-        conn.commit()
-        cur.close()
-        conn.close()
-
-    def tearDown(self):
-        super().tearDown()
-        conn = psycopg2.connect(self.conf)
-        cur = conn.cursor()
-        cur.execute("DROP TABLE test;")
-        cur.execute("DROP TABLE migration;")
-        conn.commit()
-        cur.close()
-        conn.close()
-
-    def test_write_once(self):
-        self.populate_file('test_inventory.yml', '''
-        postgres:
-            dbname: test
-            user: test
-            password: test
-            host: localhost
-            port: 5433
-        ''')
+    def test_stop(self):
         self.populate_file('main.yaml', '''---
             variables:
-                hash: 'migration_1'
+                counter: 0
             steps:
-                - postgres:
-                    request:
-                        conf: '{{ postgres }}'
-                        query: "select count(*) from migration where hash = '{{ hash }}';"
-                    register: {result: '{{ OUTPUT }}'}
+                - echo: {from: '{{ counter + 1 }}', register: {counter: '{{ OUTPUT }}'}}
                 - stop: 
                     if: 
-                        equals: {the: '{{ result }}', is: 1}
-                - postgres:
-                    actions:
-                        - request:
-                            conf: '{{ postgres }}'
-                            query: 'insert into test(id, num) values(3, 3);'
-                        - request:
-                            conf: '{{ postgres }}'
-                            query: "insert into migration(id, hash) values(1, '{{ hash }}');"
+                        equals: '{{ counter > 1 }}'
+                - echo: {from: '{{ counter + 1 }}', register: {counter: '{{ OUTPUT }}'}}
+                - stop: 
+                    if: 
+                        equals: '{{ counter > 1 }}'
+                - echo: {from: '{{ counter + 1 }}', register: {counter: '{{ OUTPUT }}'}}
+                - check: {equals: {the: '{{ counter }}', is: 2}}
             ''')
-        runner = Runner(self.test_dir, join(self.test_dir, 'main.yaml'), join(self.test_dir, 'test_inventory.yml'))
+        runner = Runner(self.test_dir, join(self.test_dir, 'main.yaml'), None)
         self.assertTrue(runner.run_tests())
-        self.assertTrue(runner.run_tests())
-        conn = psycopg2.connect(self.conf)
-        cur = conn.cursor()
-        cur.execute("select count(*) from test")
-        response = cur.fetchall()
-        conn.commit()
-        cur.close()
-        conn.close()
-        self.assertEqual([(1,)], response)
 
     def test_stop_from_include(self):
-        self.populate_file('test_inventory.yml', '''
-        postgres:
-            dbname: test
-            user: test
-            password: test
-            host: localhost
-            port: 5433
-        ''')
         self.populate_file('main.yaml', '''---
         include: 
             file: migration.yaml
             as: migrate
+        variables:
+            counter: 0
         steps:
-            - run: migrate.check
-            - postgres:
-                request:
-                    conf: '{{ postgres }}'
-                    query: "insert into test(id, num) values(3, 3);"
-            - run: migrate.commit
+            - run: migrate
+            - check: {equals: {the: '{{ counter }}', is: 2}}
+            - run: migrate
+            - check: {equals: {the: '{{ counter }}', is: 2}}
         ''')
         self.populate_file('migration.yaml', '''---
         steps:
-            - postgres:
-                request:
-                    conf: '{{ postgres }}'
-                    query: "select count(*) from migration where hash = '{{ TEST_NAME }}';"
-                register: {result: '{{ OUTPUT }}'}
-                tag: check
-                name: 'check_migration_{{ TEST_NAME }}'
+            - echo: {from: '{{ counter + 1 }}', register: {counter: '{{ OUTPUT }}'}}
             - stop: 
                 if: 
-                    equals: {the: '{{ result }}', is: 1}
-                tag: check
-                name: 'stop_if_already_run_{{ TEST_NAME }}'
-            - postgres:
-                request:
-                    conf: '{{ postgres }}'
-                    query: "insert into migration(id, hash) values(1, '{{ TEST_NAME }}');"
-                tag: commit
-                name: 'commit_migration_{{ TEST_NAME }}'
+                    equals: '{{ counter > 1 }}'
+            - echo: {from: '{{ counter + 1 }}', register: {counter: '{{ OUTPUT }}'}}
+            - stop: 
+                if: 
+                    equals: '{{ counter > 1 }}'
+            - echo: {from: '{{ counter + 1 }}', register: {counter: '{{ OUTPUT }}'}}
+            - check: {equals: {the: '{{ counter }}', is: 2}}
         ''')
-        runner = Runner(self.test_dir, join(self.test_dir, 'main.yaml'), join(self.test_dir, 'test_inventory.yml'))
+        runner = Runner(self.test_dir, join(self.test_dir, 'main.yaml'), None)
         self.assertTrue(runner.run_tests())
-        self.assertTrue(runner.run_tests())
-        conn = psycopg2.connect(self.conf)
-        cur = conn.cursor()
-        cur.execute("select count(*) from test")
-        response = cur.fetchall()
-        conn.commit()
-        cur.close()
-        conn.close()
-        self.assertEqual([(1,)], response)
