5d44a0bd34f7f1bc98505ef5d2048c6798d47282
Valerii Tikhonov <com.tihon@mail.ru>
2018-07-26 19:53:23 +0200

    fix tests

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,10 @@
 from os.path import join
 
+from catcher.utils.module_utils import load_external_actions
+
 TEST_DIR = 'test/tmp'
+
+load_external_actions('catcher.steps')
 
 
 def get_test_dir(testcase: str) -> str:
