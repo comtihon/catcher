3c0c48f7922e40ae7f0a97c51500d00d7517f1db
Val <valerii.tikhonov@smava.de>
2020-02-04 17:57:12 +0100

    actualize python classifiers

--- setup.py
+++ setup.py
@@ -25,10 +25,9 @@ setup(name=catcher.APPNAME,
       classifiers=[
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Apache Software License',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Topic :: Software Development :: Testing'
       ],
       extras={'compose': ["docker-compose==1.24.*"]}
