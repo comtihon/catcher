d5820ffaa093e68d25cd88dd4f8128f2b414a6cf
Val <valerii.tikhonov@smava.de>
2020-03-20 20:38:46 +0100

    switch agent

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(name=catcher.APPNAME,
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Topic :: Software Development :: Testing'
       ],
       extras={'compose': ["docker-compose==1.24.*"]}
