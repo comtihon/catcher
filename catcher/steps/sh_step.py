5ca5d9f2db7f7672e53fea1883e71c613e3de4fa
Val <valerii.tikhonov@gmail.com>
2020-03-30 20:04:37 +0200

    add sh step, prettify internal_modules doc

---
+++ catcher/steps/sh_step.py
@@ -0,0 +1,46 @@
+import subprocess
+
+from catcher.steps.step import Step, update_variables
+from catcher.utils.misc import fill_template
+from catcher.utils.logger import debug
+
+
+class Sh(Step):
+    """
+    Run shell command and return output.
+
+    :Input:
+
+    - command: Command to run.
+    - path: Path to be used as a root for the command. *Optional*.
+    - return_code: expected return code. *Optional*. 0 is default.
+
+    :Examples:
+
+    List current directory
+    ::
+
+        - sh:
+            command: 'ls -la'
+
+    """
+
+    def __init__(self, command=None, path=None, return_code=0, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self._cmd = command
+        self._path = path
+        self._return_code = return_code
+
+    @update_variables
+    def action(self, includes: dict, variables: dict) -> dict or tuple:
+        cmd = fill_template(self._cmd, variables)
+        process = subprocess.Popen(cmd.split(' '),
+                                   cwd=fill_template(self._path, variables),
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   universal_newlines=True)
+        stdout, stderr = process.communicate()
+        if process.returncode != int(fill_template(self._return_code, variables)):
+            debug('Process return code {}.\nStderr is {}\nStdout is {}'.format(process.returncode, stderr, stdout))
+            raise Exception
+        return variables, stdout
