fdbae980aaba368ad18d9708cea2c9f7b89a5713
Val <valerii.tikhonov@gmail.com>
2020-03-30 11:35:58 +0200

    use one ts in tests

--- test/core/filters_test.py
+++ test/core/filters_test.py
@@ -130,7 +130,7 @@ def _not_a_fun(arg):
     def test_date_time_filters(self):
         self.populate_file('main.yaml', '''---
                                 variables:
-                                    date_time: '2020-03-12 11:11:11.111'
+                                    date_time: '2020-03-30 11:21:47.455790'
                                     timestamp: 1585560107.45579
                                 steps:
                                     - echo: {from: '{{ date_time | astimestamp }}', to: one.output}
@@ -138,5 +138,5 @@ def _not_a_fun(arg):
                                 ''')
         runner = Runner(self.test_dir, join(self.test_dir, 'main.yaml'), None)
         self.assertTrue(runner.run_tests())
-        self.assertTrue(check_file(join(self.test_dir, 'one.output'), "1584007871.111"))
+        self.assertTrue(check_file(join(self.test_dir, 'one.output'), "1585560107.45579"))
         self.assertTrue(check_file(join(self.test_dir, 'two.output'), '2020-03-30 11:21:47.455790'))
