3517736676b72970aa7f9041fcc0048bdd5380bf
Val <valerii.tikhonov@gmail.com>
2020-09-10 19:43:44 +0200

    use max width auto

--- docs/conf.py
+++ docs/conf.py
@@ -101,9 +101,9 @@ html_theme_options = {
     'github_button': True,
     'badge_branch': 'master',
     'page_width': 'auto',
+    'body_max_width': 'auto',
     'travis_button': True
 }
-
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
