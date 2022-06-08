687d760b3566e3d0370b480e60f7d4d9d44bc1a2
Val <valerii.tikhonov@gmail.com>
2022-06-08 18:20:48 +0200

    added long_description to pypi

--- setup.py
+++ setup.py
@@ -8,9 +8,16 @@ def get_requirements() -> list:
         return f.readlines()
 
 
+def load_readme() -> str:
+    with open('Readme.rst', 'r') as f:
+        return f.read()
+
+
 setup(name=catcher.APPNAME,
       version=catcher.APPVSN,
       description='Microservices automated test tool.',
+      long_description=load_readme(),
+      long_description_content_type='text/x-rst',
       author=catcher.APPAUTHOR,
       author_email='valerii.tikhonov@gmail.com',
       url='https://github.com/comtihon/catcher',
