ff890e2e6def7ea93a52f0ef8fc833e4fb44039e
Val <valerii.tikhonov@airteam.ai>
2023-05-10 18:11:43 +0200

    drop 3.7

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(name=catcher.APPNAME,
       classifiers=[
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Apache Software License',
-          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
           'Programming Language :: Python :: 3.10',
